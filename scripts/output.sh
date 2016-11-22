db26bfd085077e52faf940bc0618d32bae01aed8
Glenn Plas <glenn@byte-consult.be>
2016-11-22 22:08:33 +0100

    fix output

--- scripts/output.sh
+++ scripts/output.sh
@@ -10,18 +10,20 @@ APP_URL=$1
 # get reachable ip from bridge
 BRIDGED_IP=$(ifconfig | grep 'inet addr' | cut -d ':' -f 2 | awk '{ print $1 }' | grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' | grep -v 10.0.2.15)
 
-echo "Vagrant Box shell provisioned!"
+echo "" ; echo "Vagrant Box shell provisioned!" ; echo ""
 
 if [ -z "$BRIDGED_IP" ]; then
    echo "Cannot find a suitable private ip to connect to the app" ; echo ""
 else
-   echo "Add this to your local /etc/hosts or equivalent file\n" 
+   echo "Add this to your local /etc/hosts or equivalent file\n" ; echo ""
 
    cat <<EOF
 ${BRIDGED_IP} ${APP_URL}
 ${BRIDGED_IP} api.${APP_URL}
 ${BRIDGED_IP} admin.${APP_URL}
 EOF
+
+echo ""
 
 fi
 
