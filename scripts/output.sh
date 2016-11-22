80446c51586c8e57e44cce715d6285a7e08dc5b4
Glenn Plas <glenn@byte-consult.be>
2016-11-22 22:06:41 +0100

    bash bug

--- scripts/output.sh
+++ scripts/output.sh
@@ -8,12 +8,12 @@ fi
 APP_URL=$1
 
 # get reachable ip from bridge
-BRIDGED_IP=ifconfig | grep 'inet addr' | cut -d ':' -f 2 | awk '{ print $1 }' | grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' | grep -v 10.0.2.15
+BRIDGED_IP=$(ifconfig | grep 'inet addr' | cut -d ':' -f 2 | awk '{ print $1 }' | grep -E '^(192\.168|10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.)' | grep -v 10.0.2.15)
 
 echo "Vagrant Box shell provisioned!"
 
-if [ -z "${BRIDGED_IP}" ]; then
-   echo "Cannot find a suitable private ip to connect to the app\n" 
+if [ -z "$BRIDGED_IP" ]; then
+   echo "Cannot find a suitable private ip to connect to the app" ; echo ""
 else
    echo "Add this to your local /etc/hosts or equivalent file\n" 
 
